ce8000a2bf2127a0147b85ba7926d5dc98c65eee
rashi_suneriya <rashisuneriya975@gmail.com>
2026-06-21 20:29:16 +0530

    ch-8

---
+++ chapter-8_Function_Recursion/01_function.py
@@ -0,0 +1,20 @@
+ # print (average)
+ 
+#  Function Definition
+
+def avg():
+  a = int(input("Enter your number: "))
+  b = int(input("Enter your number: "))
+  c = int(input("Enter your number: "))
+  
+  average = (a + b + c)/3
+  print(average)
+  
+  
+  # function call
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
---
+++ chapter-8_Function_Recursion/03_function_with_argument.py
@@ -0,0 +1,8 @@
+def goodDay(name, ending):
+  print("Good Day, " + name)
+  print(ending)
+  
+  
+goodDay("Rashi", "Thank you")
+goodDay("yash", "Thank you")
+goodDay("diya", "Thank you")
